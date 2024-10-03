44fc9815c1e9de4d7006e89396ffe8b018881cce
Richard <richard232manansala@gmail.com>
2024-10-03 18:24:57 +0800

    Added network dep to app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ android {
 
 dependencies {
 
+    implementation(project(":network"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
