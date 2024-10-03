1439435f336fa22ea01f0a6fb18937769fcacc83
Richard <richard232manansala@gmail.com>
2024-10-03 18:09:02 +0800

    Added NetworkModule

---
+++ network/src/main/java/com/coderizzard/network/data/NetworkModule.kt
@@ -0,0 +1,27 @@
+package com.coderizzard.network.data
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun providesRetrofit() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://quizizz.com/")
+            .build()
+    }
+
+
+}
