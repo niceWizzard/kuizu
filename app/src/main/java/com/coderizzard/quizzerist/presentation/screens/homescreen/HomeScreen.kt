fe13e1d4f0ae4118859e495948475a440fe9462e
Richard <richard232manansala@gmail.com>
2024-10-03 22:00:32 +0800

    Added vertical scrolling to the quiz raw json text

--- app/src/main/java/com/coderizzard/quizzerist/presentation/screens/homescreen/HomeScreen.kt
+++ app/src/main/java/com/coderizzard/quizzerist/presentation/screens/homescreen/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -31,6 +32,7 @@ private fun HomeScreenContent(
         Text("Home Screen")
         Text(
             quizJson,
+            modifier = Modifier.verticalScroll(rememberScrollState())
         )
     }
 }
