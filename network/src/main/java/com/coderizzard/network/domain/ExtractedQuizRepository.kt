11d96896c2603f3dcd98103c76785f8345a2fb87
Richard <richard232manansala@gmail.com>
2024-10-04 19:55:28 +0800

    Removed the GET annotation

--- network/src/main/java/com/coderizzard/network/domain/ExtractedQuizRepository.kt
+++ network/src/main/java/com/coderizzard/network/domain/ExtractedQuizRepository.kt
@@ -1,11 +1,11 @@
 package com.coderizzard.network.domain
 
 import com.coderizzard.network.data.repository.ApiResponse
-import retrofit2.http.GET
 
 interface ExtractedQuizRepository {
 
-    @GET("quiz/{id}")
     suspend fun extractQuizRaw(id : String) : ApiResponse<String>
+
+
 
 }
