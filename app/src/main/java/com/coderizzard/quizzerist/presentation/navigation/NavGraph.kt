935e0bdeb6afac411ad4d95ec304a8b3a2821f4c
Richard <richard232manansala@gmail.com>
2024-10-05 07:48:17 +0800

    Passed the localContext to hiltViewModel instead of creating the viewModel outside of the navgraph

--- app/src/main/java/com/coderizzard/quizzerist/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/coderizzard/quizzerist/presentation/navigation/NavGraph.kt
@@ -1,5 +1,6 @@
 package com.coderizzard.quizzerist.presentation.navigation
 
+import android.app.Activity
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Home
@@ -7,7 +8,9 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModelStoreOwner
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,7 +21,7 @@ import com.coderizzard.quizzerist.presentation.screens.homescreen.HomeScreenView
 fun NavGraph(
     navController: NavHostController
 ) {
-    val homeScreenViewModel: HomeScreenViewModel = hiltViewModel()
+
     NavHost(
         navController = navController,
         startDestination = NavRoute.QUIZ.route
@@ -26,7 +29,7 @@ fun NavGraph(
         composable(NavRoute.QUIZ.route) {
             HomeScreen(
                 navController = navController,
-                homeScreenViewModel = homeScreenViewModel
+
             )
         }
         composable(NavRoute.SETTINGS.route) {
--- app/src/main/java/com/coderizzard/quizzerist/presentation/screens/homescreen/HomeScreen.kt
+++ app/src/main/java/com/coderizzard/quizzerist/presentation/screens/homescreen/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.coderizzard.quizzerist.presentation.screens.homescreen
 
+import android.app.Activity
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,15 +13,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(
     navController: NavController,
-    homeScreenViewModel: HomeScreenViewModel
 ) {
-
+    val activity = LocalContext.current as Activity as ViewModelStoreOwner
+    val homeScreenViewModel: HomeScreenViewModel = hiltViewModel(activity)
     val quizJson by homeScreenViewModel.quizJson.collectAsState()
     HomeScreenContent(
         quizJson = quizJson.toString()
