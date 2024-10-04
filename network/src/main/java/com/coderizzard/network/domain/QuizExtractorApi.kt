8626b92e5b4173f0de6b34f5b73f08c0b10a5eb4
Richard <richard232manansala@gmail.com>
2024-10-04 19:54:50 +0800

    Added an func in QuizExtractorApi that returns an ExtractedQuiz type

--- network/src/main/java/com/coderizzard/network/domain/QuizExtractorApi.kt
+++ network/src/main/java/com/coderizzard/network/domain/QuizExtractorApi.kt
@@ -1,5 +1,7 @@
 package com.coderizzard.network.domain
 
+import com.coderizzard.network.data.model.ExtractedQuiz
+import com.coderizzard.network.data.repository.ApiResponse
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.GET
@@ -10,5 +12,8 @@ interface QuizExtractorApi {
 
     @GET("quiz/{id}")
     suspend fun getQuizByIdRaw(@Path("id") quizId : String) : Response<ResponseBody>
+
+    @GET("quiz/{id}")
+    suspend fun extractQuizById(@Path("id") quizId: String) : ExtractedQuiz
 
 }
