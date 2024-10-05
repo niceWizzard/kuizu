e17de92f702926129b6e75174b455a79e525ad99
Richard <richard232manansala@gmail.com>
2024-10-05 08:46:05 +0800

    Catch Http400 error and parse the error body to return the message insteead of generic RetrofitError

--- network/src/main/java/com/coderizzard/network/data/repository/ExtractedQuizRepositoryImpl.kt
+++ network/src/main/java/com/coderizzard/network/data/repository/ExtractedQuizRepositoryImpl.kt
@@ -3,6 +3,9 @@ package com.coderizzard.network.data.repository
 import com.coderizzard.network.data.model.ExtractedQuiz
 import com.coderizzard.network.domain.ExtractedQuizRepository
 import com.coderizzard.network.domain.QuizExtractorApi
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class ExtractedQuizRepositoryImpl @Inject constructor(
@@ -24,8 +27,22 @@ class ExtractedQuizRepositoryImpl @Inject constructor(
     override suspend fun extractQuizById(quizId: String): ApiResponse<ExtractedQuiz> {
         return try {
             ApiResponse.Success(extractorApi.extractQuizById(quizId))
+        } catch (e : HttpException) {
+            try {
+                if(e.code() == 400) {
+                    val body = e.response()?.errorBody()
+                    if(body != null) {
+                        val bodyJson = JsonParser.parseString(body.string()).asJsonObject
+                        val message = bodyJson.get("message").asString
+                        return ApiResponse.Error("Quizizz error: $message")
+                    }
+                }
+                ApiResponse.Error("Some error happened: \n${e}")
+            } catch (e : Exception) {
+                ApiResponse.Error("Some error happened: \n${e}")
+            }
         } catch (e : Exception) {
-            ApiResponse.Error("Some error happened: \n${e.message}")
+            ApiResponse.Error("Some error happened: \n${e}")
         }
     }
 }
