92ec3940fc0e215a46858c8728d6d72b55fd688c
Richard <richard232manansala@gmail.com>
2024-10-06 19:26:11 +0800

    Used hasRoute ext func of NavDestination instead of checking for equality of route string and class names

--- app/src/main/java/com/coderizzard/quizzerist/presentation/TopBar.kt
+++ app/src/main/java/com/coderizzard/quizzerist/presentation/TopBar.kt
@@ -1,7 +1,5 @@
 package com.coderizzard.quizzerist.presentation
 
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -10,6 +8,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.coderizzard.quizzerist.presentation.navigation.NavRoute
@@ -21,10 +21,11 @@ fun TopBar(navController: NavController) {
 
     val backStackEntry by navController.currentBackStackEntryAsState()
     backStackEntry?.let { entry ->
-        val a = entry.destination.route
-        val routeName  = routeList.find {
-            a?.contains(it.javaClass.name.replace("$", ".")) ?: false
-        }?.displayName ?: a ?: "Cannot find route name"
+        val routeName  = routeList.find { navRoute ->
+            entry.destination.hierarchy.any {
+                it.hasRoute(navRoute::class)
+            }
+        }?.displayName ?: "Cannot find route name"
         TopAppBar(
             title = { Text(routeName)},
         )
