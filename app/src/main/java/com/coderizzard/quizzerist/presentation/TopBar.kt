cfe0cb9788795a17ceffb6c8e30bf10dbc66696d
Richard <richard232manansala@gmail.com>
2024-10-07 08:44:48 +0800

    Changed TopBar depending on route

--- app/src/main/java/com/coderizzard/quizzerist/presentation/TopBar.kt
+++ app/src/main/java/com/coderizzard/quizzerist/presentation/TopBar.kt
@@ -1,10 +1,18 @@
 package com.coderizzard.quizzerist.presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hasRoute
@@ -20,14 +28,39 @@ fun TopBar(navController: NavController) {
 
     val backStackEntry by navController.currentBackStackEntryAsState()
     backStackEntry?.let { entry ->
-        val routeName  = routeList.find { navRoute ->
+        val v = routeList.find { navRoute ->
             entry.destination.hierarchy.any {
                 it.hasRoute(navRoute::class)
             }
-        }?.displayName ?: "Cannot find route name"
-        TopAppBar(
-            title = { Text(routeName)},
-        )
+        }
+        if (v != null) {
+            TopAppBar(
+                title = { Text(v.displayName)},
+            )
+        }
+        else {
+            TopAppBar(
+                title = { },
+                actions = {
+                    Row(
+                        horizontalArrangement = Arrangement.Start,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        IconButton(
+                            onClick = {
+                                navController.popBackStack()
+                            }
+                        ) {
+                            Icon(
+                                Icons.AutoMirrored.Default.ArrowBack,
+                                contentDescription = "Back"
+                            )
+                        }
+                    }
+                }
+            )
+        }
+
     }
 }
 
