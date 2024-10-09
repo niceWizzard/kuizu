9a198ee258a2a187b1426cc0b859f08045cc58d8
Richard <richard232manansala@gmail.com>
2024-10-09 21:22:43 +0800

    Added default random uuid string to param id in question entities

--- database/src/main/java/com/coderizzard/core/data/database/model/question/IdentificationQuestionEntity.kt
+++ database/src/main/java/com/coderizzard/core/data/database/model/question/IdentificationQuestionEntity.kt
@@ -3,11 +3,12 @@ package com.coderizzard.core.data.database.model.question
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.UUID
 
 @Entity
 data class IdentificationQuestionEntity(
     @PrimaryKey
-    override val id: String,
+    override val id: String = UUID.randomUUID().toString(),
     @ColumnInfo("text")
     override val text: String,
     @ColumnInfo("point")
