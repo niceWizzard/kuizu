92ce9deb7140dbd578e2680414b1f5d6262288d9
Richard <richard232manansala@gmail.com>
2024-10-09 21:24:49 +0800

    Added the entities in AppDatabase annotation

--- database/src/main/java/com/coderizzard/core/data/database/AppDatabase.kt
+++ database/src/main/java/com/coderizzard/core/data/database/AppDatabase.kt
@@ -2,9 +2,16 @@ package com.coderizzard.core.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.coderizzard.core.data.database.model.QuizEntity
+import com.coderizzard.core.data.database.model.question.IdentificationQuestionEntity
+import com.coderizzard.core.data.database.model.question.MultipleChoiceQuestionEntity
 
 @Database(
-    entities = [],
+    entities = [
+        QuizEntity::class,
+        MultipleChoiceQuestionEntity::class,
+        IdentificationQuestionEntity::class,
+    ],
     version = 1
 )
 abstract class AppDatabase : RoomDatabase() {
