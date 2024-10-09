699ec373fc2657418cb72e15e0a5b83301e388e4
Richard <richard232manansala@gmail.com>
2024-10-09 09:38:09 +0800

    Added a simple view for the ExtractedQuiz

--- app/src/main/java/com/coderizzard/quizzerist/presentation/screens/homescreen/HomeScreen.kt
+++ app/src/main/java/com/coderizzard/quizzerist/presentation/screens/homescreen/HomeScreen.kt
@@ -4,9 +4,14 @@ import android.app.Activity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -19,11 +24,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.navigation.NavController
+import com.coderizzard.network.data.model.ExtractedIdentificationQuestion
+import com.coderizzard.network.data.model.ExtractedMultipleChoiceQuestion
 
 @Composable
 fun HomeScreen(
@@ -102,7 +112,65 @@ private fun HomeScreenContent(
                 }
             }
             is SearchQuizState.Success -> {
-                Text(searchQuizState.data.toString())
+                val quiz = searchQuizState.data
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .verticalScroll(rememberScrollState()),
+                    verticalArrangement = Arrangement
+                        .spacedBy(12.dp)
+                ) {
+                    Text(
+                        quiz.name,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Row {
+                        Text(
+                            "Created by: ${quiz.author}",
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Light
+                        )
+                        Text("Created at: ${quiz.createdAt}",
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Light
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(32.dp))
+                    quiz.questionList.map { q ->
+                        Card {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 12.dp, vertical = 8.dp),
+                            ) {
+                                Text(
+                                    q.text,
+                                    fontSize = 18.sp,
+                                )
+                                Spacer(Modifier.height(12.dp))
+                                when(q) {
+                                    is ExtractedIdentificationQuestion -> {
+                                        Text("Answer: ${q.answer}")
+                                    }
+                                    is ExtractedMultipleChoiceQuestion -> {
+                                        Column {
+                                            q.options.mapIndexed { index, opt ->
+                                                val isCorrect = q.answer.contains(index)
+                                                Text(buildString {
+                                                    if (isCorrect)
+                                                        append( "Correct - ")
+                                                    append(opt)
+                                                })
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
             }
 
             is SearchQuizState.Invalid -> {
