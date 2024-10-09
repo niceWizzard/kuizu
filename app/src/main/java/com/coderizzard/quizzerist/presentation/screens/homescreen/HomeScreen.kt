5bb3d2da94257a3ba7eb221744cd48a8a78fdb72
Richard <richard232manansala@gmail.com>
2024-10-09 22:16:35 +0800

    Added quiz list to homeScreen

--- app/src/main/java/com/coderizzard/quizzerist/presentation/screens/homescreen/HomeScreen.kt
+++ app/src/main/java/com/coderizzard/quizzerist/presentation/screens/homescreen/HomeScreen.kt
@@ -1,59 +1,47 @@
 package com.coderizzard.quizzerist.presentation.screens.homescreen
 
 import android.app.Activity
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.navigation.NavController
-import com.coderizzard.network.data.model.ExtractedIdentificationQuestion
-import com.coderizzard.network.data.model.ExtractedMultipleChoiceQuestion
+import com.coderizzard.database.data.database.model.QuizEntity
 
 @Composable
 fun HomeScreen(
     navController: NavController,
 ) {
-
+    val activity = LocalContext.current as Activity as ViewModelStoreOwner
+    val homeScreenViewModel: HomeScreenViewModel = hiltViewModel(activity)
+    val quizList by homeScreenViewModel.allQuizzes.collectAsState(emptyList())
     HomeScreenContent(
-
+        quizList
     )
 }
 
 @Composable
-private fun HomeScreenContent() {
-
+private fun HomeScreenContent(quizList: List<QuizEntity>) {
+    Column {
+        Text("All Quizzes")
+        Column {
+            quizList.map {
+                Text(it.toString())
+            }
+            quizList.isEmpty().let {
+                Text("No quizzes yet.")
+            }
+        }
+    }
 }
 
 @Preview
 @Composable
 private fun HomeScreenPreview() {
-    HomeScreenContent(
-
-    )
+    HomeScreenContent(quizList = emptyList())
 }
--- app/src/main/java/com/coderizzard/quizzerist/presentation/screens/homescreen/HomeScreenViewModel.kt
+++ app/src/main/java/com/coderizzard/quizzerist/presentation/screens/homescreen/HomeScreenViewModel.kt
@@ -1,15 +1,17 @@
 package com.coderizzard.quizzerist.presentation.screens.homescreen
 
 import androidx.lifecycle.ViewModel
+import com.coderizzard.database.domain.repository.QuizRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeScreenViewModel  @Inject constructor() : ViewModel() {
+class HomeScreenViewModel  @Inject constructor(
+    private val quizRepository: QuizRepository
+) : ViewModel() {
 
 
-
-
+    val allQuizzes = quizRepository.getAll()
 }
 
 
