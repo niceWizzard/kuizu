17b77cccf4b56449761afff3b7c052b40ead6816
Richard <richard232manansala@gmail.com>
2024-10-09 21:24:33 +0800

    Added QuizEntity

---
+++ database/src/main/java/com/coderizzard/core/data/database/model/QuizEntity.kt
@@ -0,0 +1,21 @@
+package com.coderizzard.core.data.database.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Entity
+data class QuizEntity (
+    @PrimaryKey
+    val id : String = UUID.randomUUID().toString(),
+    @ColumnInfo("name")
+    val name : String,
+    @ColumnInfo("author")
+    val author : String,
+    @ColumnInfo("image_link")
+    val imageLink : String,
+    @ColumnInfo("created_at")
+    val createdAt : LocalDateTime,
+)
