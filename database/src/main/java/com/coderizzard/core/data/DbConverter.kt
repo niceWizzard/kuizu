c8e2c30fe8ae31f979114ab6d015844ef50155d8
Richard <richard232manansala@gmail.com>
2024-10-09 21:32:32 +0800

    Added LocalDateTime <-> String convertion in db

--- database/src/main/java/com/coderizzard/core/data/DbConverter.kt
+++ database/src/main/java/com/coderizzard/core/data/DbConverter.kt
@@ -3,6 +3,7 @@ package com.coderizzard.core.data
 import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.time.LocalDateTime
 
 class DbConverter  {
 
@@ -17,7 +18,15 @@ class DbConverter  {
         return Gson().fromJson(value, listType)
     }
 
+    @TypeConverter
+    fun fromStringToLocalDateTime(value : String?) : LocalDateTime {
+        return LocalDateTime.parse(value)
+    }
 
+    @TypeConverter
+    fun fromLocalDateTimeToString(v : LocalDateTime) : String {
+        return v.toString()
+    }
 
 
 }
