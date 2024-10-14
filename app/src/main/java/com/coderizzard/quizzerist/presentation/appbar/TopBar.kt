6fe372c2598f7461b25d2e46a5640d2aa8a1638f
Richard <richard232manansala@gmail.com>
2024-10-14 18:18:05 +0800

    Changed package name of AppBar to reflect its file route

--- app/src/main/java/com/coderizzard/quizzerist/presentation/appbar/TopBar.kt
+++ app/src/main/java/com/coderizzard/quizzerist/presentation/appbar/TopBar.kt
@@ -1,4 +1,4 @@
-package com.coderizzard.quizzerist.presentation
+package com.coderizzard.quizzerist.presentation.appbar
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -7,8 +7,6 @@ import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.coderizzard.quizzerist.presentation.appbar.HomeAppBar
-import com.coderizzard.quizzerist.presentation.appbar.RootAppBar
 import com.coderizzard.core.data.navigation.HomeRoute
 import com.coderizzard.core.data.navigation.RootRoute
 
