b8d847484993535390e7f9e9cc0111ec7bc6fb4b
Richard <richard232manansala@gmail.com>
2024-10-14 17:42:06 +0800

    Added deleteQuiz in QuizDao and Repository

--- database/src/main/java/com/coderizzard/database/data/database/QuizDao.kt
+++ database/src/main/java/com/coderizzard/database/data/database/QuizDao.kt
@@ -1,6 +1,7 @@
 package com.coderizzard.database.data.database
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.coderizzard.database.data.database.model.QuizEntity
@@ -17,4 +18,6 @@ interface QuizDao {
     @Query("SELECT * FROM QuizEntity AS ent WHERE ent.id = :id")
     fun getById(id: String) : Flow<QuizEntity>
 
+    @Query("DELETE FROM QuizEntity WHERE id = :id")
+    fun deleteQuiz(id : String)
 }
