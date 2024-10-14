45c100e22be03e172d7c2d84ee263436667c383c
Richard <richard232manansala@gmail.com>
2024-10-14 17:12:49 +0800

    Added NavManager class

---
+++ core/src/main/java/com/coderizzard/core/data/navigation/NavigationManager.kt
@@ -0,0 +1,22 @@
+package com.coderizzard.core.data.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.toRoute
+import javax.inject.Inject
+
+
+class NavigationManager @Inject constructor() {
+    lateinit var navController: NavController
+
+    fun navigateTo(
+        route : NavRoute
+    ) {
+        navController.navigate(route)
+    }
+
+    inline fun <reified T : NavRoute> getRouteData(): T? {
+        return navController.currentBackStackEntry?.toRoute<T>()
+
+    }
+
+}
