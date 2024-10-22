ebf8309d4eaa1e3a1c6829701df9db591d7d5e8e
Richard <richard232manansala@gmail.com>
2024-10-23 00:21:13 +0800

    Added a Floating Action Button in the QuizRoute

--- app/src/main/java/com/coderizzard/quizzerist/presentation/action_button/RootRouteActionButton.kt
+++ app/src/main/java/com/coderizzard/quizzerist/presentation/action_button/RootRouteActionButton.kt
@@ -1,17 +1,33 @@
 package com.coderizzard.quizzerist.presentation.action_button
 
+import android.widget.Toast
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import com.coderizzard.core.data.navigation.RootRoute
 
 @Composable
 fun RootRouteActionButton(
     route : RootRoute,
 ) {
+    val context = LocalContext.current
     when(route) {
+        is RootRoute.Quiz -> {
+            FloatingActionButton(
+                onClick = {
+                    Toast.makeText(context , "To be implemented", Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Icon(
+                    Icons.Default.PlayArrow,
+                    "Start quiz"
+                )
+            }
+        }
         else -> {}
     }
 }
