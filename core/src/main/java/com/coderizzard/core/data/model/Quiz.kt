4f8ad18f810acd8ed7d6735878abfdd79acabd69
Richard <richard232manansala@gmail.com>
2024-10-23 15:47:34 +0800

    Removed the image property of the Quiz data class

--- core/src/main/java/com/coderizzard/core/data/model/Quiz.kt
+++ core/src/main/java/com/coderizzard/core/data/model/Quiz.kt
@@ -1,12 +1,10 @@
 package com.coderizzard.core.data.model
 
-import android.media.Image
 import com.coderizzard.core.data.model.question.Question
 import java.time.LocalDateTime
 
 data class Quiz(
     val id : String,
-    val image: Image? = null,
     val name : String,
     val author : String,
     val createdAt : LocalDateTime,
