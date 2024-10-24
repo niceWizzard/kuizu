8b4e41c58ef911d39896fcc7d629e8a3188cbd6b
Richard <richard232manansala@gmail.com>
2024-10-24 13:01:30 +0800

    Now deletes the images when also deleting the quiz

--- quiz/src/main/java/com/coderizzard/quiz/presentation/screen/quiz/QuizScreenViewModel.kt
+++ quiz/src/main/java/com/coderizzard/quiz/presentation/screen/quiz/QuizScreenViewModel.kt
@@ -6,6 +6,7 @@ import com.coderizzard.core.data.navigation.NavigationManager
 import com.coderizzard.core.data.model.Quiz
 import com.coderizzard.core.data.navigation.RootRoute
 import com.coderizzard.database.domain.repository.QuizRepository
+import com.coderizzard.quiz.domain.repository.ImageManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -17,6 +18,7 @@ import javax.inject.Inject
  internal class QuizScreenViewModel @Inject constructor(
     private val quizRepository: QuizRepository,
     val navigationManager: NavigationManager,
+     private val imageManager: ImageManager
 ) : ViewModel() {
     private val _quizState = MutableStateFlow<QuizUiState>(QuizUiState.Loading)
     private var _routeParams : RootRoute.Quiz = navigationManager.getRouteData<RootRoute.Quiz>() ?:   throw Exception("Reached Quiz(#id) without a quiz route")
@@ -35,7 +37,15 @@ import javax.inject.Inject
 
     fun deleteCurrentQuiz() {
         viewModelScope.launch {
-            quizRepository.deleteQuiz(routeParams().id)
+            val id = routeParams().id
+            val quiz = quizRepository.getById(id)
+            imageManager.deleteImage(quiz.localImagePath)
+            quiz.questions.forEach{
+                if(it.localImagePath.isNotBlank()) {
+                    imageManager.deleteImage(it.localImagePath)
+                }
+            }
+            quizRepository.deleteQuiz(id)
         }
     }
 
