122a16cdb64983c4750ce1955ba32ffc553d8187
Richard <richard232manansala@gmail.com>
2024-10-24 12:31:15 +0800

    Displayed the localImage instead of the remoteImage in the Quiz

--- quiz/src/main/java/com/coderizzard/quiz/presentation/screen/quiz_list/QuizList.kt
+++ quiz/src/main/java/com/coderizzard/quiz/presentation/screen/quiz_list/QuizList.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -63,7 +64,7 @@ internal fun QuizList(quizList: QuizListState, onQuizClick: (id: String) -> Unit
                             )
                         ) {
                             AsyncImage(
-                                model = quiz.imageLink,
+                                model = quiz.localImagePath,
                                 contentDescription = "Nice",
                             )
                             Text(
