92803f0969d8072378ffa48c7f12894229dd06dd
Richard <richard232manansala@gmail.com>
2024-10-24 12:30:55 +0800

    Saved the returned quiz from saveQuizImage instead of the original one

--- quiz/src/main/java/com/coderizzard/quiz/data/ImageManagerImpl.kt
+++ quiz/src/main/java/com/coderizzard/quiz/data/ImageManagerImpl.kt
@@ -1,55 +1,69 @@
 package com.coderizzard.quiz.data
 
-import android.content.ContentValues
 import android.content.Context
-import android.os.Build
-import android.os.Environment
-import android.provider.MediaStore
 import android.util.Log
 import com.coderizzard.core.data.model.Quiz
 import com.coderizzard.network.data.repository.ApiResponse
 import com.coderizzard.network.domain.ExtractedQuizRepository
 import com.coderizzard.quiz.domain.repository.ImageManager
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import java.io.File
-import java.io.FileOutputStream
+import java.io.IOException
 import java.io.InputStream
-import java.text.SimpleDateFormat
-import java.util.Locale
+import java.util.UUID
 import javax.inject.Inject
 
 class ImageManagerImpl @Inject constructor(
     private val extractorRepository: ExtractedQuizRepository
 ) : ImageManager {
-    override suspend fun saveQuizImages(quiz: Quiz, context: Context) {
+    override suspend fun saveQuizImages(quiz: Quiz, context: Context): Quiz {
+        var newQuiz = quiz
         if (quiz.imageLink.isNotBlank()) {
-            saveQuizMainImage(quiz, context)
+            newQuiz = saveQuizMainImage(quiz, context)
         }
+        return newQuiz
     }
 
-    private suspend fun saveQuizMainImage(quiz: Quiz, context: Context) {
-        when (val resp = extractorRepository.extractImage(quiz.imageLink)) {
+    private suspend fun saveQuizMainImage(quiz: Quiz, context: Context) : Quiz {
+        return when (val resp = extractorRepository.extractImage(quiz.imageLink)) {
             is ApiResponse.Error -> {
                 Log.e(
                     "AddQuizViewModel.saveImages",
                     "Couldn't extract the quiz image. \n${resp.message}"
                 )
+                quiz
             }
+
             is ApiResponse.Success -> {
-                resp.value.body()?.byteStream()?.let { inputStream ->
-                    saveImageToStorage(context, inputStream)
-                }
+                val quizImagePath = resp.value.body()?.byteStream()?.use { inputStream ->
+                    saveImageToInternalStorage(
+                        context,
+                        inputStream,
+                        "quiz-${quiz.remoteId}_${UUID.randomUUID()}.jpg"
+                    )
+                } ?: return quiz
+                return quiz.copy(localImagePath = quizImagePath)
             }
         }
     }
 
+    private fun saveImageToInternalStorage(context: Context, inputStream: InputStream, fileName: String): String {
+        val imagesDir = File(context.filesDir, "images")
 
-    private suspend fun saveImageToStorage(context : Context, stream : InputStream) {
-        val fileName = SimpleDateFormat(
-            "yyyy-MM-dd-HH-mm-ss-SSS",
-            Locale.ENGLISH
-        ).format(System.currentTimeMillis())
+        if (!imagesDir.exists()) {
+            imagesDir.mkdir()
+        }
+
+        val imageFile = File(imagesDir, fileName)
+
+        return try {
+            imageFile.outputStream().use { outputStream ->
+                inputStream.copyTo(outputStream)
+            }
+            imageFile.absolutePath
+        } catch (e: IOException) {
+            e.printStackTrace()
+            ""
+        }
     }
 
 
--- quiz/src/main/java/com/coderizzard/quiz/domain/repository/ImageManager.kt
+++ quiz/src/main/java/com/coderizzard/quiz/domain/repository/ImageManager.kt
@@ -4,6 +4,6 @@ import android.content.Context
 import com.coderizzard.core.data.model.Quiz
 
 interface ImageManager {
-    suspend fun saveQuizImages(quiz : Quiz, context : Context)
+    suspend fun saveQuizImages(quiz : Quiz, context : Context) : Quiz
 
 }
