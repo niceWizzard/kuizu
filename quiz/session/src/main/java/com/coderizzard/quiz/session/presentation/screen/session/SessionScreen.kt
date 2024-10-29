086674a93d24a89bb874bd0f6dfefed894c65f9c
Richard <richard232manansala@gmail.com>
2024-10-29 22:06:24 +0800

    Moved some composables into their own function

--- quiz/session/src/main/java/com/coderizzard/quiz/session/presentation/screen/session/SessionScreen.kt
+++ quiz/session/src/main/java/com/coderizzard/quiz/session/presentation/screen/session/SessionScreen.kt
@@ -1,40 +1,18 @@
 package com.coderizzard.quiz.session.presentation.screen.session
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.coderizzard.core.data.AsyncData
 import com.coderizzard.core.data.model.Quiz
@@ -42,8 +20,9 @@ import com.coderizzard.core.data.model.question.IdentificationQuestion
 import com.coderizzard.core.data.model.question.MCOption
 import com.coderizzard.core.data.model.question.MCQuestion
 import com.coderizzard.core.data.model.session.QuizSession
-import com.coderizzard.core.data.toAnnotatedString
-import com.coderizzard.core.presentation.expandable_image.ExpandableImage
+import com.coderizzard.quiz.session.presentation.screen.session.screen.AnsweringScreen
+import com.coderizzard.quiz.session.presentation.screen.session.screen.FinishedScreen
+import com.coderizzard.quiz.session.presentation.screen.session.screen.StartScreen
 import java.time.LocalDateTime
 
 @Composable
@@ -90,188 +69,20 @@ private fun Content(
                     val session = sessionData.data
                     when(uiState){
                         is SessionUiState.Answering -> {
-                            val question = uiState.q
-                            Card(
-                                colors = when(answeringState) {
-                                    AnsweringState.Correct -> {
-                                        CardDefaults.cardColors(
-                                            contentColor = MaterialTheme.colorScheme.primary,
-                                        )
-                                    }
-                                    is AnsweringState.IncorrectIdentificationAnswer,
-                                    is AnsweringState.IncorrectMCAnswer -> {
-                                        CardDefaults.cardColors(
-                                            contentColor = MaterialTheme.colorScheme.error,
-                                        )
-                                    }
-                                    AnsweringState.Unanswered -> CardDefaults.cardColors()
-                                }
-                            ) {
-                                Column(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .heightIn(
-                                            min = 128.dp,
-                                        )
-                                        .padding(24.dp),
-                                    verticalArrangement = Arrangement.Center,
-                                    horizontalAlignment = Alignment.CenterHorizontally,
-                                ) {
-                                    Row(
-                                        modifier = Modifier
-                                            .fillMaxWidth(),
-                                        horizontalArrangement = Arrangement.spacedBy(
-                                            12.dp, alignment = Alignment.CenterHorizontally,
-                                        )
-                                    ) {
-                                        Text(
-                                            "Question: ${session.currentQuestionIndex + 1}/${session.questionOrder.size}",
-                                            fontWeight = FontWeight.Light,
-                                            textAlign = TextAlign.Center,
-                                        )
-                                        Text(
-                                            "Correct: $score",
-                                            fontWeight = FontWeight.Light,
-                                            textAlign = TextAlign.Center,
-                                        )
-                                    }
-                                    Spacer(
-                                        modifier = Modifier.height(18.dp)
-                                    )
-                                    if(question.localImagePath.isNotBlank()){
-                                        ExpandableImage(
-                                            modifier = Modifier.fillMaxWidth(),
-                                            imageUrl = question.localImagePath,
-                                            contentDescription = "Current question image"
-                                        )
-                                    }
-                                    Text(
-                                        question.text.toAnnotatedString(),
-                                        fontSize = 18.sp,
-                                        modifier = Modifier.verticalScroll(rememberScrollState())
-
-                                    )
-                                }
-                            }
-                            when(question) {
-                                is IdentificationQuestion -> {
-                                    TextField(
-                                        value = identificationFieldData,
-                                        onValueChange = {
-                                            onEvent(ScreenEvent.IdentificationFieldUpdate(it))
-                                        },
-                                        modifier = Modifier.fillMaxWidth(),
-                                        label = {Text("Answer")},
-                                        singleLine = true,
-                                        enabled = answeringState == AnsweringState.Unanswered,
-                                    )
-                                    ElevatedButton(
-                                        onClick = {
-                                            onEvent(ScreenEvent.IdentificationAnswer(identificationFieldData))
-                                        },
-                                        modifier = Modifier.fillMaxWidth(),
-                                        enabled = answeringState == AnsweringState.Unanswered,
-                                    ) {
-                                        Text("Submit")
-                                    }
-                                    when(answeringState) {
-                                        AnsweringState.Correct -> {
-                                            Text(
-                                                "Correct",
-                                                color = MaterialTheme.colorScheme.secondary
-                                            )
-                                        }
-                                        is AnsweringState.IncorrectIdentificationAnswer -> {
-                                            Text(
-                                                "Correct: ${question.answer}",
-                                                color = MaterialTheme.colorScheme.error,
-                                            )
-                                        }
-                                        is AnsweringState.IncorrectMCAnswer,
-                                        AnsweringState.Unanswered -> {}
-                                    }
-                                }
-                                is MCQuestion -> {
-                                    Column(
-                                        verticalArrangement = Arrangement.spacedBy(6.dp)
-                                    ) {
-                                        question.options.map { opt ->
-                                            ElevatedButton(
-                                                onClick = {
-                                                    onEvent(ScreenEvent.MCAnswer(listOf(opt.remoteId)))
-                                                },
-                                                modifier = Modifier.fillMaxWidth(),
-                                                colors = when(answeringState) {
-                                                    AnsweringState.Correct -> {
-                                                        if(question.answer.contains(opt.remoteId))
-                                                            ButtonDefaults.buttonColors(
-                                                                disabledContainerColor = MaterialTheme.colorScheme.secondary,
-                                                                disabledContentColor = MaterialTheme.colorScheme.onSecondary,
-                                                            )
-                                                        else
-                                                            ButtonDefaults.buttonColors()
-                                                    }
-                                                    is AnsweringState.IncorrectMCAnswer -> {
-                                                        if(answeringState.answers.contains(opt.remoteId)) {
-                                                            ButtonDefaults.buttonColors(
-                                                                disabledContainerColor = Color.Red,
-                                                                disabledContentColor = Color.Black,
-                                                            )
-                                                        }
-                                                        else if(question.answer.contains(opt.remoteId))
-                                                            ButtonDefaults.buttonColors(
-                                                                disabledContainerColor =  MaterialTheme.colorScheme.primary,
-                                                                disabledContentColor = MaterialTheme.colorScheme.onPrimary,
-                                                            )
-                                                        else
-                                                            ButtonDefaults.buttonColors()
-                                                    }
-                                                    is AnsweringState.IncorrectIdentificationAnswer,
-                                                    AnsweringState.Unanswered ->ButtonDefaults.buttonColors()
-                                                },
-                                                enabled = answeringState == AnsweringState.Unanswered
-                                            ) {
-                                                Text(
-                                                    opt.text.toAnnotatedString(),
-                                                    modifier = Modifier.padding(16.dp),
-                                                    fontSize = 16.sp,
-                                                )
-                                            }
-                                        }
-                                    }
-                                }
-                            }
+                            AnsweringScreen(
+                                uiState,
+                                answeringState,
+                                session,
+                                score,
+                                identificationFieldData,
+                                onEvent
+                            )
                         }
                         SessionUiState.Finished -> {
-                            Text("Finished!")
+                            FinishedScreen()
                         }
                         SessionUiState.Default -> {
-                            Box(
-                                modifier = Modifier.fillMaxSize(),
-                                contentAlignment = Alignment.Center,
-                            ) {
-                                Card {
-                                    Column(
-                                        modifier = Modifier
-                                            .padding(
-                                                12.dp
-                                            )
-                                            .fillMaxWidth(),
-                                        verticalArrangement = Arrangement.spacedBy(6.dp)
-                                    ) {
-                                        Text(session.quiz.name)
-                                        Text(session.quiz.author)
-                                        ElevatedButton(
-                                            modifier = Modifier.fillMaxWidth(),
-                                            onClick = {
-                                                onEvent(ScreenEvent.Start)
-                                            }
-                                        ) {
-                                            Text("Start")
-                                        }
-                                    }
-                                }
-                            }
+                            StartScreen(session, onEvent)
                         }
                     }
                 }
@@ -279,6 +90,7 @@ private fun Content(
         }
     }
 }
+
 
 @Preview
 @Composable
---
+++ quiz/session/src/main/java/com/coderizzard/quiz/session/presentation/screen/session/screen/FinishedScreen.kt
@@ -0,0 +1,9 @@
+package com.coderizzard.quiz.session.presentation.screen.session.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun FinishedScreen() {
+    Text("Finished!")
+}
