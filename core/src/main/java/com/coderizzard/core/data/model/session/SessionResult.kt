489557f4700a2a00350f257529d70a6af948a295
Richard <richard232manansala@gmail.com>
2024-10-31 16:11:35 +0800

    Made the id auto default to random uuid

--- core/src/main/java/com/coderizzard/core/data/model/session/SessionResult.kt
+++ core/src/main/java/com/coderizzard/core/data/model/session/SessionResult.kt
@@ -1,9 +1,10 @@
 package com.coderizzard.core.data.model.session
 
 import java.time.LocalDateTime
+import java.util.UUID
 
 data class SessionResult(
-    val id : String,
+    val id : String = UUID.randomUUID().toString(),
     val quizId : String,
     val totalPoints : Int,
     val marks : Int,
