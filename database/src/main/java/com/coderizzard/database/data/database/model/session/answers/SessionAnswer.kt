c71d59ae6476cd39ab00371a0af48636a1738be6
Richard <richard232manansala@gmail.com>
2024-11-01 18:02:37 +0800

    Added converter function for SessionAnswerEntity

--- database/src/main/java/com/coderizzard/database/data/database/model/session/answers/SessionAnswer.kt
+++ database/src/main/java/com/coderizzard/database/data/database/model/session/answers/SessionAnswer.kt
@@ -1,6 +1,15 @@
 package com.coderizzard.database.data.database.model.session.answers
 
+import com.coderizzard.core.data.model.session.answer.SessionAnswer
+
 sealed interface SessionAnswer {
+    fun toSessionAnswer() : SessionAnswer {
+        return when(this) {
+            is IdentificationAnswerEntity -> this.toIdentificationAnswer()
+            is MCQuestionAnswerEntity -> this.toMCQuestionAnswer()
+        }
+    }
+
     val quizId : String
     val questionId : String
     val isCorrect : Boolean
