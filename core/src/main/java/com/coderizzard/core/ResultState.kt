df49cd6e6fc7675b1c093bba1cad1533641b929b
Richard <richard232manansala@gmail.com>
2024-11-01 18:09:04 +0800

    Added converter function toAsyncData

--- core/src/main/java/com/coderizzard/core/ResultState.kt
+++ core/src/main/java/com/coderizzard/core/ResultState.kt
@@ -1,6 +1,15 @@
 package com.coderizzard.core
 
+import com.coderizzard.core.data.AsyncData
+
 sealed interface ResultState<out T> {
     data class Success<T>(val data: T) : ResultState<T>
     data class Error(val message: String, val exception: Throwable? = null) : ResultState<Nothing>
+
+    fun toAsyncData() : AsyncData<T> {
+        return when(this) {
+            is Error -> AsyncData.Error(message, exception)
+            is Success -> AsyncData.Success(data)
+        }
+    }
 }
