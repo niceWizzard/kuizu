29c73299b57c6f6e41ea0ceadd1dd3b9207c317d
Richard <richard232manansala@gmail.com>
2024-11-04 21:59:09 +0800

    Improved the contrast of the secondary color from the primary

--- app/src/main/java/com/coderizzard/kuizu/ui/theme/Color.kt
+++ app/src/main/java/com/coderizzard/kuizu/ui/theme/Color.kt
@@ -3,7 +3,7 @@ package com.coderizzard.kuizu.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val PurpleGrey80 = Color(0xFF544E60)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
--- app/src/main/java/com/coderizzard/kuizu/ui/theme/Theme.kt
+++ app/src/main/java/com/coderizzard/kuizu/ui/theme/Theme.kt
@@ -8,12 +8,14 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    onSecondary = Color(0xFFE4E4E4)
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -40,10 +42,10 @@ fun KuizuTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
