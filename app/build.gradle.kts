9b74154966bbbcbda43047523882b641191f7d2a
Richard <richard232manansala@gmail.com>
2024-11-05 21:31:36 +0800

    Updated app versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.coderizzard.kuizu"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.1-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
